3d2a35f967b4e09234a356de4d2a087b0f3de300
michaelgobz <raniahgobz@outlook.com>
2023-07-13 14:48:22 +0300

    first unit test

--- tests/customer.test.cs
+++ tests/customer.test.cs
@@ -1,10 +1,24 @@
-namespace tests;
+using ACM.BL;
+namespace ACM.tests;
 
 [TestClass]
 public class CustomerTest
 {
     [TestMethod]
-    public void FullNameTest()
+    public void TestFullNameIsValid()
     {
+
+        // Arrange
+        var customer = new Customer
+        {
+            FirstName = "Michael",
+            LastName = "Goboola"
+        };
+    
+        // Act
+        var expected = "Michael, Goboola";
+
+        // Assert
+        Assert.AreEqual(expected, customer.FullName);
     }
 }
